aed873760bf8d13a52d7df096b3724df77fcdc92
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-27 21:52:41 -0300

    react Router Dom and first configurations

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import VideoList from './components/Videos/VideoList';
+import VideoForm from './components/Videos/VideoForm';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={VideoList} />
+        <Route path="/new-video" component={VideoForm} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
